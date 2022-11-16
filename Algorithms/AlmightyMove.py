99a15a9cff1a3b2bf92ecd48a1d80ad096c35929
EsCharl <63916811+EsCharl@users.noreply.github.com>
2022-11-16 17:32:12 +0000

    AM basic done (even needs backup)

--- Algorithms/AlmightyMove.py
+++ Algorithms/AlmightyMove.py
@@ -27,17 +27,42 @@ class AlmightyMove(SnakeLogic):
                     else:
                         self.template_path_0.append([[x, y], Directions.UP])
 
-            for i in range(len(self.template_path_0)):
-                if self.template_path_0[i][0][1] == SQUARE_AMOUNT - 1:
-                    self.template_path_0[i][1] = Directions.LEFT
+            for i in self.template_path_0.copy():
+                # this part removes the top part (other than x = 0,1)
+                if i[0] == [0, SQUARE_AMOUNT]:
+                    i[1] = Directions.UP
+                elif i[0] == [0, 0]:
+                    i[1] = Directions.RIGHT
+                elif i[0][1] == SQUARE_AMOUNT - 1 and not i[0] == [0, SQUARE_AMOUNT-1]:
+                    i[1] = Directions.LEFT
+                elif i[0][0] % 2 and i[0][1] == SQUARE_AMOUNT - 2 and not i[0] == [SQUARE_AMOUNT-2, SQUARE_AMOUNT-2]:
+                    i[1] = Directions.RIGHT
+                elif not i[0][0] % 2 and i[0][1] == 1 and not i[0] == [0, 1]:
+                    i[1] = Directions.RIGHT
+                elif i[0][0] in range(2, SQUARE_AMOUNT-1) and i[0][1] == 0:
+                    self.template_path_0.remove(i)
 
             # right template
             for y in range(SQUARE_AMOUNT):
                 for x in range(1, SQUARE_AMOUNT):
                     if x % 2:
-                        self.template_path_0.append([[x, y], Directions.DOWN])
+                        self.template_path_1.append([[x, y], Directions.DOWN])
                     else:
-                        self.template_path_0.append([[x, y], Directions.UP])
+                        self.template_path_1.append([[x, y], Directions.UP])
+
+            for i in self.template_path_1.copy():
+                if i[0] == [SQUARE_AMOUNT - 2, SQUARE_AMOUNT - 1]:
+                    i[1] = Directions.RIGHT
+                if i[0] == [1, 0]:
+                    i[1] = Directions.DOWN
+                elif i[0][1] == 0:
+                    i[1] = Directions.LEFT
+                elif i[0][1] == 1 and i[0][0] < SQUARE_AMOUNT - 1 and not i[0][0] % 2:
+                    i[1] = Directions.RIGHT
+                elif i[0][1] == SQUARE_AMOUNT - 2 and i[0][0] % 2 and not i[0] == [SQUARE_AMOUNT - 2, SQUARE_AMOUNT - 2]:
+                    i[1] = Directions.RIGHT
+                elif i[0][0] in range(1, SQUARE_AMOUNT - 2) and i[0][1] == SQUARE_AMOUNT - 1:
+                    self.template_path_1.remove(i)
 
         else:
             # this is for even number game-board
@@ -60,6 +85,7 @@ class AlmightyMove(SnakeLogic):
                     i[1] = Directions.RIGHT
 
     def move(self):
+        preferred_direction = None
         filtered_steps = []
         potential_steps = self.generate_all_potential_steps()
 
@@ -70,14 +96,60 @@ class AlmightyMove(SnakeLogic):
 
         # if the game-board is odd
         if SQUARE_AMOUNT % 2:
-            if self.body[0] == [0,1]:
+
+            # this is to switch the template
+            if self.body[0] == [1, 0]:
                 if self.template:
                     self.template = 0
                 else:
                     self.template = 1
 
+            if self.template:
+                for i in self.template_path_1:
+                    if i[0] == self.body[0]:
+                        preferred_direction = i[1]
+                        break
+
+            else:
+                print("t", self.body[0])
+                print(self.template_path_0)
+                for i in self.template_path_0:
+                    if i[0] == self.body[0]:
+                        preferred_direction = i[1]
+                        break
+                print(filtered_steps)
+                print(self.check_direction_possible(filtered_steps, preferred_direction))
+
+            preferred_step = self.check_direction_possible(filtered_steps, preferred_direction)
+
+            ## needs adding changing
+
+            # # if can't get a preferred_step
+            # if not preferred_step and filtered_steps:
+            #     if self.body[0][1] < SQUARE_AMOUNT - 1 and [self.body[0][0] + 1, self.body[0][1]] in filtered_steps:
+            #         preferred_step = [self.body[0][0] + 1, self.body[0][1]]
+            #     elif self.body[0][1] == SQUARE_AMOUNT - 1 and [self.body[0][0], self.body[0][1] - 1] in filtered_steps:
+            #         preferred_step = [self.body[0][0], self.body[0][1] - 1]
+            #
+            # if not preferred_step and len(filtered_steps) == 1:
+            #     preferred_step = filtered_steps[0]
+            # elif not preferred_step and filtered_steps:
+            #     if preferred_direction == Directions.DOWN:
+            #         preferred_direction = Directions.UP
+            #     elif preferred_direction == Directions.UP:
+            #         preferred_direction = Directions.DOWN
+            #     elif preferred_direction == Directions.LEFT:
+            #         preferred_direction = Directions.RIGHT
+            #     elif preferred_direction == Directions.RIGHT:
+            #         preferred_direction = Directions.LEFT
+            #     preferred_step = self.check_direction_possible(filtered_steps, preferred_direction)
+            #
+            # # this is just in case it can't find any steps
+            # if not preferred_step:
+            #     preferred_step = random.choice(filtered_steps)
+
+
         else:
-            preferred_direction = None
             for i in self.template_path_0:
                 if i[0] == self.body[0]:
                     preferred_direction = i[1]
@@ -108,43 +180,6 @@ class AlmightyMove(SnakeLogic):
             # this is just in case it can't find any steps
             if not preferred_step:
                 preferred_step = random.choice(filtered_steps)
-
-            # # this parts changes the direction when it is on the bottom edges
-            # if self.body[0] == [SQUARE_AMOUNT - 1, SQUARE_AMOUNT - 1] or self.body[0] == [0, SQUARE_AMOUNT - 1]:
-            #     if self.vertical_direction == Directions.DOWN:
-            #         self.vertical_direction = Directions.UP
-            #
-            # # this part changes the vertical direction when the head reaches the top row and the direction is up.
-            # if self.body[0][1] == 0 and self.vertical_direction == Directions.UP:
-            #     self.vertical_direction = Directions.DOWN
-            # # this part changes the vertical direction when the head reaches the 2nd bottom row and the direction is down
-            # elif self.body[0][1] == SQUARE_AMOUNT - 2 and self.vertical_direction == Directions.DOWN:
-            #     self.vertical_direction = Directions.UP
-            #
-            # # this part changes the horizontal when the head reaches the furthest right, and it is on the right direction
-            # if self.body[0][0] == SQUARE_AMOUNT - 1 and self.horizontal_direction == Directions.RIGHT:
-            #     self.horizontal_direction = Directions.LEFT
-            #
-            # # this part changes the horizontal when the head reaches the furthest left, and it is on the left direction
-            # if self.body[0][0] == 0 and self.horizontal_direction == Directions.LEFT:
-            #     self.horizontal_direction = Directions.RIGHT
-            #
-            # # this part moves decide the moves for the snake
-            # if self.body[0][1] == SQUARE_AMOUNT - 1 and not self.body[0][0] == 0:
-            #     step = self.decide_horizontal(self.horizontal_direction)
-            # elif self.body[0] == [SQUARE_AMOUNT - 1, SQUARE_AMOUNT - 2]:
-            #     self.vertical_direction = Directions.DOWN
-            #     step = self.decide_vertical(self.vertical_direction)
-            # elif self.body[0] == [0, SQUARE_AMOUNT - 1]:
-            #     step = self.decide_vertical(self.vertical_direction)
-            # elif (self.body[0][1] + 1 in self.body or ((self.body[0][1] == SQUARE_AMOUNT - 2 or self.body[0][
-            #     1] == 0) and not self.moved_horizontal)) and self.horizontal_direction == Directions.RIGHT:
-            #     step = self.decide_horizontal(self.horizontal_direction)
-            #     self.moved_horizontal = True
-            # else:
-            #     step = self.decide_vertical(self.vertical_direction)
-            #     self.moved_horizontal = False
-
 
         # this part checks if the step being made is it require a reset.
         if preferred_step:
