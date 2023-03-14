704d9b52ff687dcc42be5e187f3d59314977a174
EsCharl <63916811+EsCharl@users.noreply.github.com>
2023-03-14 16:21:28 +0000

    more graph analysis

--- StatisticalAnalysis.py
+++ StatisticalAnalysis.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-import numpy
 import numpy as np
 
 AccAlgo = open("Evaluation/Score/AccumAlgo.txt", "r")
@@ -11,30 +10,13 @@ acc_round_nums = []
 acc_resets = []
 acc_food_gained = []
 
-# acc_round_nums_std = []
-# acc_resets_std = []
-# acc_food_gained_std = []
-#
-# acc_round_nums_avg = []
-# acc_resets_avg = []
-# acc_food_gained_avg = []
-
 for v in acc_result_list:
     i = v.split(", ")
     acc_round_nums.append(int(i[0]))
     acc_resets.append(int(i[1]))
     acc_food_gained.append(int(i[2]))
 
-# acc_round_nums_avg = sum(acc_round_nums) / len(acc_round_nums)
-# acc_round_nums_std = numpy.std(acc_round_nums)
-#
-# acc_resets_avg = sum(acc_resets) / len(acc_resets)
-# acc_resets_std = numpy.std(acc_resets)
-#
-# acc_food_gained_avg = sum(acc_food_gained) / len(acc_food_gained)
-# acc_food_gained_std = numpy.std(acc_food_gained)
-
-Model = open("Evaluation/Score/Model.txt", "r")
+Model = open("Evaluation/Score/Model (2).txt", "r")
 
 model_result_list = Model.read().split("\n")
 model_result_list.pop()
@@ -43,31 +25,11 @@ model_round_nums = []
 model_resets = []
 model_food_gained = []
 
-# model_round_nums_std = []
-# model_resets_std = []
-# model_food_gained_std = []
-#
-# model_round_nums_avg = []
-# model_resets_avg = []
-# model_food_gained_avg = []
-
 for d in model_result_list:
     i = d.split(", ")
     model_round_nums.append(int(i[0]))
     model_resets.append(int(i[1]))
     model_food_gained.append(int(i[2]))
-
-# model_round_nums_avg = sum(model_round_nums) / len(model_round_nums)
-# model_round_nums_std = numpy.std(model_round_nums)
-#
-# model_resets_avg = sum(model_resets) / len(model_resets)
-# model_resets_std = numpy.std(model_resets)
-#
-# model_food_gained_avg = sum(model_food_gained) / len(model_food_gained)
-# model_food_gained_std = numpy.std(model_food_gained)
-
-# Creating axes instance
-# ax = fig.add_axes([0, 0, 1, 1])
 
 round_nums = [np.array(model_round_nums), np.array(acc_round_nums)]
 reset_nums = [np.array(model_resets), np.array(acc_resets)]
@@ -75,22 +37,110 @@ food_gained_nums = [np.array(model_food_gained), np.array(acc_food_gained)]
 
 # Creating plot
 plt.boxplot(round_nums)
-plt.title("rounds per test")
+plt.title("rounds per test (higher the better)")
 plt.xticks([1, 2], ["Model", "Acc Algo"])
 
 # show plot
 plt.show()
 
 plt.boxplot(reset_nums)
-plt.title("number of resets (100 tests)")
+plt.title("number of resets (100 tests), (lower the better)")
 plt.xticks([1, 2], ["Model", "Acc Algo"])
 
 # show plot
 plt.show()
 
 plt.boxplot(food_gained_nums)
-plt.title("number of food gained (100 tests)")
+plt.title("number of food gained (100 tests), (higher the better)")
 plt.xticks([1, 2], ["Model", "Acc Algo"])
+
+# show plot
+plt.show()
+ratio_final = [np.array(model_food_gained) / np.array(model_resets), np.array(acc_food_gained) / np.array(acc_resets)]
+plt.boxplot(ratio_final)
+plt.title("ratio between food gained and reset (higher the better)")
+plt.xticks([1, 2], ["Model", "Acc Algo"])
+
+# show plot
+plt.show()
+
+round_nums = [np.array(model_round_nums), np.array(acc_round_nums)]
+reset_nums = [np.array(model_resets), np.array(acc_resets)]
+food_gained_nums = [np.array(model_food_gained), np.array(acc_food_gained)]
+
+# Creating plot
+plt.boxplot(round_nums)
+plt.title("rounds per test (higher the better)")
+plt.xticks([1, 2], ["Model", "Acc Algo"])
+
+# show plot
+plt.show()
+
+plt.boxplot(reset_nums)
+plt.title("number of resets (100 tests), (lower the better)")
+plt.xticks([1, 2], ["Model", "Acc Algo"])
+
+# show plot
+plt.show()
+
+plt.boxplot(food_gained_nums)
+plt.title("number of food gained (100 tests), (higher the better)")
+plt.xticks([1, 2], ["Model", "Acc Algo"])
+
+# show plot
+plt.show()
+ratio_final = [np.array(model_food_gained) / np.array(model_resets), np.array(acc_food_gained) / np.array(acc_resets)]
+plt.boxplot(ratio_final)
+plt.title("ratio between food gained and reset (higher the better)")
+plt.xticks([1, 2], ["Model", "Acc Algo"])
+
+# show plot
+plt.show()
+
+Model_test = open("Evaluation/Score/model gen moves first.txt", "r")
+
+model_test_result_list = Model_test.read().split("\n")
+model_test_result_list.pop()
+
+model_test_round_nums = []
+model_test_resets = []
+model_test_food_gained = []
+
+for d in model_test_result_list:
+    i = d.split(", ")
+    model_test_round_nums.append(int(i[0]))
+    model_test_resets.append(int(i[1]))
+    model_test_food_gained.append(int(i[2]))
+
+round_nums.append(np.array(model_test_round_nums))
+reset_nums.append(np.array(model_test_resets))
+food_gained_nums.append(np.array(model_test_food_gained))
+
+# Creating plot
+plt.boxplot(round_nums)
+plt.title("rounds per test (higher the better)")
+plt.xticks([1, 2, 3], ["Model", "Acc Algo", "Model (gen move first)"])
+
+# show plot
+plt.show()
+
+plt.boxplot(reset_nums)
+plt.title("number of resets (100 tests), (lower the better)")
+plt.xticks([1, 2, 3], ["Model", "Acc Algo", "Model (gen move first)"])
+
+# show plot
+plt.show()
+
+plt.boxplot(food_gained_nums)
+plt.title("number of food gained (100 tests), (higher the better)")
+plt.xticks([1, 2, 3], ["Model", "Acc Algo", "Model (gen move first)"])
+
+# show plot
+plt.show()
+ratio_final.append(np.array(model_test_food_gained) / np.array(model_test_resets))
+plt.boxplot(ratio_final)
+plt.title("ratio between food gained and reset (higher the better)")
+plt.xticks([1, 2, 3], ["Model", "Acc Algo", "Model (gen move first)"])
 
 # show plot
 plt.show()
