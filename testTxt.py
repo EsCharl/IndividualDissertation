e44dedf3605ded4140dee1a9e62483cfcc8ebbe0
EsCharl <63916811+EsCharl@users.noreply.github.com>
2023-03-22 00:39:31 +0000

    removal of the test files

--- testTxt.py
+++
@@ -1,7 +0,0 @@
-FILE_DIR = "F:/test/05_02_2023 13_04_23/"
-WINNER_FILE = FILE_DIR + "winners.txt"
-f = open(WINNER_FILE, "r")
-text = f.read().split("\n")
-while "" in text:
-    text.remove("")
-print(text)
--- testpickle.py
+++
@@ -1,5 +0,0 @@
-import pickle
-
-if __name__ == '__main__':
-    file = open("F:/test/05_02_2023 17_55_57/Steps/A-Star/332.pickle", 'rb')
-    print(pickle.load(file))
